7e72f93cc829d0dec24d1b8ff4f440c9bfa86d0b
Ross Henderson <rossliamhenderson@gmail.com>
2024-06-09 14:23:24 +0100

    Basic set up of App

---
+++ greenkeeping/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'greenkeeping',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ greenkeeping/src/pages/Tab1.tsx
@@ -0,0 +1,33 @@
+import { IonContent, IonHeader, IonPage, IonMenu, IonMenuButton, IonButtons, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab1.css';
+
+const Tab1: React.FC = () => {
+  return (
+(
+    <>
+      <IonMenu contentId="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Menu Content</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">This is the menu content.</IonContent>
+      </IonMenu>
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Menu</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">Tap the button in the toolbar to open the menu.</IonContent>
+      </IonPage>
+    </>
+  )
+)
+}
+
+export default Tab1;
